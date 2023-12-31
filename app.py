1bebc82be47e7c6638e54ca75c55e20367b5dd4b
Ajay <dancerworld60@gmail.com>
2023-12-31 07:22:54 +0530

    running n ubuntu

--- app.py
+++ app.py
@@ -37,6 +37,6 @@ def predict(input_data:PredictionInput):
     # Return the prediction
     return {"prediction": prediction}
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
